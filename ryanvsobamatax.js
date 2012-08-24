9aa87b85ff11570434094e7b47f4c5af29bf58f0
Ben Breedlove <benbreedlove@gmail.com>
2012-08-23 18:10:29 -0700

    make many js fixes, remaining is fix up input style, fix calculation to be based on % of tax obligation, not income

--- ryanvsobamatax.js
+++ ryanvsobamatax.js
@@ -3,209 +3,209 @@ var tax_brackets = [
         bracket: 10000,
         amount_in_bracket: 10000,
         ryan: {
-            single: 1.2,
-            joint: 2.5,
-            children: 6.3
+            single: '1.2',
+            joint: '2.5',
+            children: '6.3'
         },
         romney: {
-            single: 1.2,
-            joint: 2.6,
-            children: 6.3
+            single: '1.2',
+            joint: '2.6',
+            children: '6.3'
         },
         obama: {
-            single: 0,
-            joint: 0.2,
-            children: 0
+            single: '0',
+            joint: '0.2',
+            children: '0'
         }
     },
     { 
         bracket: 20000,
         amount_in_bracket: 10000,
         ryan: {
-            single: 0.3,
-            joint: 2.4,
-            children: 5.1
+            single: '0.3',
+            joint: '2.4',
+            children: '5.1'
         },
         romney: {
-            single: 0.1,
-            joint: 2.5,
-            children: 5.1
+            single: '0.1',
+            joint: '2.5',
+            children: '5.1'
         },
         obama: {
-            single: 0,
-            joint: 0.1,
-            children: 0
+            single: '0',
+            joint: '0.1',
+            children: '0'
         }
     },
     { 
         bracket: 30000,
         amount_in_bracket: 10000,
         ryan: {
-            single: -0.5,
-            joint: 1.4,
-            children: 2.1
+            single: '-0.5',
+            joint: '1.4',
+            children: '2.1'
         },
         romney: {
-            single: -0.7,
-            joint: 1.5,
-            children: 2
+            single: '-0.7',
+            joint: '1.5',
+            children: '2'
         },
         obama: {
-            single: 0,
-            joint: 0,
-            children: -0.2
+            single: '0',
+            joint: '0',
+            children: '-0.2'
         }
     },
     { 
         bracket: 40000,
         amount_in_bracket: 10000,
         ryan: {
-            single: -1.3,
-            joint: 0.6,
-            children: 0.7
+            single: '-1.3',
+            joint: '0.6',
+            children: '0.7'
         },
         romney: {
-            single: -1.3,
-            joint: 0.4,
-            children: 0.5
+            single: '-1.3',
+            joint: '0.4',
+            children: '0.5'
         },
         obama: {
-            single: 0,
-            joint: 0,
-            children: -0.2,
+            single: '0',
+            joint: '0',
+            children: '-0.2'
         }
     },
     { 
         bracket: 50000,
         amount_in_bracket: 10000,
         ryan: {
-            single: -2,
-            joint: 0.2,
-            children: 0.1
+            single: '-2',
+            joint: '0.2',
+            children: '0.1'
         },
         romney: {
-            single: -1.8,
-            joint: -0.1,
-            children: -0.1
+            single: '-1.8',
+            joint: '-0.1',
+            children: '-0.1'
         },
         obama: {
-            single: 0,
-            joint: 0,
-            children: 0.2
+            single: '0',
+            joint: '0',
+            children: '0.2'
         }
     },
     { 
         bracket: 75000,
         amount_in_bracket: 25000,
         ryan: {
-            single: -2.1,
-            joint: -0.9,
-            children: -0.9
+            single: '-2.1',
+            joint: '-0.9',
+            children: '-0.9'
         },
         romney: {
-            single: -2.6,
-            joint: -1.1,
-            children: -1
+            single: '-2.6',
+            joint: '-1.1',
+            children: '-1'
         },
         obama: {
-            single: 0.1,
-            joint: 0,
-            children: -0.1
+            single: '0.1',
+            joint: '0',
+            children: '-0.1'
         }
     },
     { 
         bracket: 100000,
         amount_in_bracket: 25000,
         ryan: {
-            single: -1.8,
-            joint: -1.9,
-            children: -1.6
+            single: '-1.8',
+            joint: '-1.9',
+            children: '-1.6'
         },
         romney: {
-            single: -3.3,
-            joint: -1.7,
-            children: -1.4
+            single: '-3.3',
+            joint: '-1.7',
+            children: '-1.4'
         },
         obama: {
-            single: 0.2,
-            joint: 0,
-            children: -0.1
+            single: '0.2',
+            joint: '0',
+            children: '-0.1'
         }
     },
     { 
         bracket: 200000,
         amount_in_bracket: 100000,
         ryan: {
-            single: -2,
-            joint: -1.9,
-            children: -1.7
+            single: '-2',
+            joint: '-1.9',
+            children: '-1.7'
         },
         romney: {
-            single: -4.1,
-            joint: -2.5,
-            children: -2.1
+            single: '-4.1',
+            joint: '-2.5',
+            children: '-2.1'
         },
         obama: {
-            single: 0.4,
-            joint: 0.1,
-            children: 0
+            single: '0.4',
+            joint: '0.1',
+            children: '0'
         }
     },
     { 
         bracket: 500000,
         amount_in_bracket: 300000,
         ryan: {
-            single: -4.5,
-            joint: -3.4,
-            children: -3.6
+            single: '-4.5',
+            joint: '-3.4',
+            children: '-3.6'
         },
         romney: {
-            single: -6.6,
-            joint: -4.9,
-            children: -4.8
+            single: '-6.6',
+            joint: '-4.9',
+            children: '-4.8'
         },
         obama: {
-            single: 2.2,
-            joint: 0.9,
-            children: 0.9
+            single: '2.2',
+            joint: '0.9',
+            children: '0.9'
         }
     },
     { 
         bracket: 1000000,
         amount_in_bracket: 500000,
         ryan: {
-            single: -6.5,
-            joint: -6.5,
-            children: -6.7
+            single: '-6.5',
+            joint: '-6.5',
+            children: '-6.7'
         },
         romney: {
-            single: -8.6,
-            joint: -6.7,
-            children: -6.7
+            single: '-8.6',
+            joint: '-6.7',
+            children: '-6.7'
         },
         obama: {
-            single: 4.9,
-            joint: 3.1,
-            children: 3.2
+            single: '4.9',
+            joint: '3.1',
+            children: '3.2'
         }
     },
     { 
         bracket: 9999999999999, // max allowed by our input
         amount_in_bracket: 9999998999999, // if we ever need this we fucked up
         ryan: {
-            single: -8.8,
-            joint: -6.5,
-            children: -6.7
+            single: '-8.8',
+            joint: '-6.5',
+            children: '-6.7'
         },
         romney: {
-            single: -10.7,
-            joint: -7.7,
-            children: -7.7
+            single: '-10.7',
+            joint: '-7.7',
+            children: '-7.7'
         },
         obama: {
-            single: 7.4,
-            joint: 5.7,
-            children:5.8
+            single: '7.4',
+            joint: '5.7',
+            children: '5.8'
         }
     }
 ];
@@ -233,7 +233,6 @@ jQuery(document).ready(function() {
         };
         
         var calculate_change = function() {
-            console.log('got here');
             running_total = {
                 obama: 0,
                 romney: 0,
@@ -246,23 +245,16 @@ jQuery(document).ready(function() {
                     : 'single';
             var over = false;
 
-            for (var i = 0; i < tax_brackets.length; i++ ) {
-                var bracket = tax_brackets[i];
-                for (politician in running_total) {
-                    if (income > bracket.bracket) { 
-                        running_total[politician] += bracket[politician][filing_status] 
-                                                        * bracket.amount_in_bracket;
-                    } else { // not the total amount in this bracket
-                        running_total[politician] += bracket[politician][filing_status] 
-                                                        * (income - bracket.bracket)
-                        over = true;
-                    }
-                }
-                if (over) {
-                    return;
+            for (politician in running_total) {
+                for (var i = 0; i < tax_brackets.length; i++ ) {
+                    if (income > tax_brackets[i].bracket) { 
+                        continue;
+                    } 
+                    running_total[politician] = parseFloat(tax_brackets[(i)][politician][filing_type]) 
+                                                    * income / 100
+                    break;
                 }
             }
-            console.log('we should never be here');
             return;
         }
 
@@ -270,9 +262,11 @@ jQuery(document).ready(function() {
             jQuery('#obama_results .dollar_results').text(commafy(running_total['obama'])); 
             jQuery('#romney_results .dollar_results').text(commafy(running_total['romney'])); 
             jQuery('#ryan_results .dollar_results').text(commafy(running_total['ryan'])); 
+//            jQuery('#annual_income_input').val(commafy(income));
         }
-        var commafy = function(num) {
-            num = num.toString();
+        var commafy = function(real_num) {
+            num = Math.abs(real_num).toFixed(2).toString();
+            num.replace('-', '');
             var numbers = num.split(/\./);
             num = numbers[0];
             var num_after_decimal = numbers.length > 1
@@ -282,7 +276,10 @@ jQuery(document).ready(function() {
             while ( regex.test(num) ) {
                 num =  num.replace(regex, '$1' + ',' + '$2');
             }
-            return num + num_after_decimal
+            return  (( real_num < 0 )
+                    ? '-'
+                    : '')
+                + '$' + num + num_after_decimal
         }
 
         var display_results_page = function() {
@@ -325,6 +322,9 @@ jQuery(document).ready(function() {
         calculator.run();
     });
     jQuery('input[type="radio"]').bind('click', function() {
+        calculator.run();
+    });
+    jQuery('input').bind('keyup', function() {
         calculator.run();
     });
 });
